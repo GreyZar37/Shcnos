e3e7fb3fd3c08aecf7d63d34e19bf17772bda3ba
GreyZar37 <52458016+GreyZar37@users.noreply.github.com>
2022-08-26 19:56:27 +0200

    Inventory

---
+++ Assets/Scripts/InventoryScp.cs
@@ -0,0 +1,42 @@
+using Cinemachine;
+using UnityEngine;
+public class InventoryScp : MonoBehaviour
+{
+    [SerializeField]
+    CinemachineVirtualCamera m_Camera;
+    [SerializeField]
+    float transition_Speed;
+
+    [SerializeField]
+    GameObject inventoryUI;
+
+    [SerializeField]
+    Animator animator;
+    bool loadInventory;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && inventoryUI.activeInHierarchy == true)
+        {
+            inventoryUI.SetActive(false);
+
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab) && inventoryUI.activeInHierarchy == false)
+        {
+            inventoryUI.SetActive(true);
+        }
+
+    }
+}
+
+
+
